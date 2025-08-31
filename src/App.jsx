ab17a8d53773e3c6f6b0c0815e5d5b96db5650bb
Bithun T Nair <bithuntnair854@gmail.com>
2025-08-31 20:33:48 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     {add(3,4)}
-    <h1>Hello</h1>
+    <h1>Helloo</h1>
     </>
   )
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,6 +6,6 @@ import App from './App'
 describe('App',()=>{
     it('Checking if H1 tag exists',()=>{
         render(<App/>);
-        expect(screen.getByRole('heading')).toHaveTextContent('Hello');
+        expect(screen.getByRole('heading')).toHaveTextContent('Helloo');
     })
 })
